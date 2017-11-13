e00acd5aa6a70c6e7980931209bedc67a580908e
Hassan Ahmed <mr.hassaan95@gmail.com>
2017-11-13 12:07:10 +0500

    Code is working well. till yet
    basic layouts has done . . . .

--- Book_barrter/app/src/main/java/com/example/admin/book_barrter/MainActivity.java
+++ Book_barrter/app/src/main/java/com/example/admin/book_barrter/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.admin.book_barrter;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void signup(View view) {
+
+        signup sign = new signup();
+        FragmentManager fm = getFragmentManager();
+        FragmentTransaction ft  = fm.beginTransaction();
+
+        // the last parameter is known as the fragment TAG ,findfragmentbyTAG, it can be used for that finding purpose...so it just a string value
+         ft.add(R.id.frames,sign,"SIGNUP");
+        ft.commit();
+
+
+
     }
 }
---
+++ Book_barrter/app/src/main/java/com/example/admin/book_barrter/signup.java
@@ -0,0 +1,40 @@
+package com.example.admin.book_barrter;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+/**
+ * Created by ADMIN on 12/11/2017.
+ */
+
+public class signup extends Fragment {
+
+    Button signup;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        //return super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.signup,container,false);
+
+        signup = (Button) view.findViewById(R.id.signupu);
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(),"sdasdasda",Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+        return view;
+
+    }
+}
