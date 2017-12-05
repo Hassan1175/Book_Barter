eba19d17d501ec73d4c76a28802d36ad264fce45
Hassan Ahmed <mr.hassaan95@gmail.com>
2017-12-05 23:19:36 +0500

    Working code,.. perfectly
    Here my code is working well.. The firebase with the user registration and authentication is working perfectly...
    But progress dialogue is not working......so it has been committed.  . .

--- Book_barrter/app/src/main/java/com/example/admin/book_barrter/login.java
+++ Book_barrter/app/src/main/java/com/example/admin/book_barrter/login.java
@@ -3,32 +3,74 @@ package com.example.admin.book_barrter;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 /**
  * Created by ADMIN on 14/11/2017.
  */
 
 public class login extends Fragment{
+    String email;
+    String password;
+
+
     Button signup1;
     FragmentManager fm;
-
+Button loginbtn;
+    ProgressDialog progressDialog;
+    FirebaseAuth firebaseAuth;
+    EditText ed1;
+    EditText  ed2;
     @Nullable
     @Override
+
+    //instances of the edit texts
+
+   // Button signup;
+
+
 
 
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
 //        return super.onCreateView(inflater, container, savedInstanceState);
 
+
         View view = inflater.inflate(R.layout.login,container,false);
 
+        //cos here we are using fragments, so for it we will use view.findview
+            ed1 = (EditText) view.findViewById(R.id.username);
+            ed2 = (EditText) view.findViewById(R.id.password);
+        loginbtn = (Button) view.findViewById(R.id.login);
         signup1 =(Button) view.findViewById(R.id.signupu);
         fm = getFragmentManager();
+
+        loginbtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+
+                // the signin is the method , which i am calling bellow, which will login the users....
+                signin();
+            }
+        });
+
+
+
         signup1.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
@@ -37,7 +79,8 @@ public class login extends Fragment{
             }
         });
 
-              return view;
+
+        return view;
 
     }
 
@@ -50,6 +93,53 @@ public class login extends Fragment{
 
       //  Toast.makeText(getActivity(),"hello how are u",Toast.LENGTH_SHORT).show();
 
+
+    }
+
+    public void signin(){
+
+        email = ed1.getText().toString().trim();
+        password = ed2.getText().toString().trim();
+
+
+
+        if(TextUtils.isEmpty(email)){
+            Toast.makeText(getActivity(),"plz enter your name",Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if(TextUtils.isEmpty(password)) {
+            Toast.makeText(getActivity(), "plz enter your password", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+
+        progressDialog.setMessage("Authenticating. ..  .Wait. .");
+        progressDialog.show();
+
+
+        firebaseAuth.signInWithEmailAndPassword(email,password)
+                .addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                       // Toast.makeText(getActivity(),"hello i am here",Toast.LENGTH_LONG).show();
+
+                        //     progressDialog.dismiss();
+                        if (task.isSuccessful()) {
+                            progressDialog.dismiss();
+                            Intent i = new Intent(getActivity(),welcome.class);
+                            startActivity(i);
+
+                            Toast.makeText(getActivity(),"Welcome welcomeee",Toast.LENGTH_SHORT).show();
+
+
+                        }
+                        else
+                            Toast.makeText(getActivity(),"Sorry somthing went wrong, try again. . ",Toast.LENGTH_SHORT).show();
+
+                    }
+                });
 
     }
 
--- Book_barrter/app/src/main/java/com/example/admin/book_barrter/signup.java
+++ Book_barrter/app/src/main/java/com/example/admin/book_barrter/signup.java
@@ -1,15 +1,25 @@
 package com.example.admin.book_barrter;
 
 import android.app.Fragment;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 /**
  * Created by ADMIN on 12/11/2017.
@@ -17,28 +27,104 @@ import android.widget.Toast;
 
 public class signup extends Fragment {
 
-    Button signp;
+    Button enroll;
 
+    EditText ed1;
+    EditText ed2;
+
+
+    ProgressDialog progressDialog;
+    FirebaseAuth firebaseAuth;
     @Nullable
-    @Override
+   @Override
+
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         //return super.onCreateView(inflater, container, savedInstanceState);
         View view = inflater.inflate(R.layout.signup,container,false);
 
-        signp = (Button) view.findViewById(R.id.registration);
 
-        signp.setOnClickListener(new View.OnClickListener() {
+        firebaseAuth = FirebaseAuth.getInstance();
+
+
+
+         ed1 = (EditText) view.findViewById(R.id.email);
+         ed2 = (EditText) view.findViewById(R.id.paswordd);
+     //   Button      btn   = (Button) view.findViewById(R.id.registration);
+
+
+
+        enroll = (Button) view.findViewById(R.id.registration);
+
+        enroll.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //Toast.makeText(getActivity(),"sdasdasda",Toast.LENGTH_LONG).show();
 
-            Intent i = new Intent(getActivity(),Home_screen.class);
-                startActivity(i);
+
+         //here i will call move method. which is creating new users omn the firebase. . . . .
+
+                move();
+
+
             }
         });
 
 
         return view;
+
+    }
+
+    public void move (){
+        String email = ed1.getText().toString().trim();
+        String password = ed2.getText().toString().trim();
+
+        Log.i("TAG", "move: "+email+"  "+password);
+
+
+        Toast.makeText(getActivity(),email,Toast.LENGTH_SHORT).show();
+        Toast.makeText(getActivity(),password,Toast.LENGTH_SHORT).show();
+/*
+        if (TextUtils.isEmpty(email)){
+            Toast.makeText(getActivity(),"Please enter your Email",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(TextUtils.isEmpty(password)) {
+            Toast.makeText(getActivity(), "plz enter your Password", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        */
+//       progressDialog.setMessage("User is Registering. .  ..");
+  //      progressDialog.show();
+
+        firebaseAuth.createUserWithEmailAndPassword(email,password)
+                .addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+
+                    Toast.makeText(getActivity(), "User has successfully enrolled", Toast.LENGTH_LONG).show();
+
+                 // progressDialog.setMessage("Book Barter Loading . . .");
+                   // progressDialog.show();
+
+
+                    //there will wait of 4 seconds
+
+
+                            Intent i = new Intent(getActivity(),Home_screen.class);
+                            startActivity(i);
+                    progressDialog.dismiss();
+
+
+
+                }
+                else
+                    Toast.makeText(getActivity(), "Something went wring. Try Again !!!", Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+
 
     }
 
